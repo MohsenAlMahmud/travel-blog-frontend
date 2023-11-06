e970b0f45256088b0bd6f751fb0259e169f1c4fa
MohsenAlMahmud <mohsen.al.mahmud@gmail.com>
2023-11-07 05:51:35 +0600

    user and bloger informetion sending backend successfully

--- src/Pages/Blogs/BlogDetails.jsx
+++ src/Pages/Blogs/BlogDetails.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { Link, useLoaderData, useParams } from "react-router-dom";
 import { AuthContext } from "../../ClientAuthentication/AuthProvider";
 import Swal from "sweetalert2";
-// import { useState, useEffect } from "react";
+
 
 const BlogDetails = () => {
 
@@ -10,10 +10,10 @@ const BlogDetails = () => {
     const { id } = useParams();
     const blog = blogs.find(blog => blog._id == id);
     console.log(blog);
-    const {user} = useContext(AuthContext);
-    console.log({user})
+    const { user } = useContext(AuthContext);
+    console.log({ user })
 
-    const handleComments = (e) =>{
+    const handleComments = (e) => {
         e.preventDefault();
 
         const form = e.target;
@@ -21,7 +21,12 @@ const BlogDetails = () => {
         const email = user?.email;
         const postComment = {
             comment,
-            email
+            email,
+            blogId: blog._id, 
+            blogTitle: blog.tittle,
+            blogImage: blog.image, 
+            blogShortDescription: blog.shortDescription,
+            blogLongDescription: blog.longDescription,
         }
         console.log(postComment);
 
@@ -32,21 +37,21 @@ const BlogDetails = () => {
             },
             body: JSON.stringify(postComment)
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            if(data.insertedId){
-                Swal.fire('Your Comment Posted Successfully')
-            }
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire('Your Comment Posted Successfully')
+                }
+            })
     }
-    
+
 
     return (
         <div>
             <div className=" bg-neutral text-neutral-content">
                 <div className="card-body items-center">
-                    <h2 className="card-title">{blog.title}</h2>
+                    <h2 className="card-title">{blog.tittle}</h2>
                     <img src="https://i.ibb.co/TKKJchx/sasha-freemind-n-Xo2-Zs-KHTHg-unsplash.jpg" alt="" />
                     <h2 className="card-title">Category : {blog.category}</h2>
                     <p>Short Description : {blog.shortDescription}</p>
@@ -60,24 +65,24 @@ const BlogDetails = () => {
             <div className="py-4">
                 <h2 className="text-3xl py-4">Comments.....</h2>
                 <form onSubmit={handleComments} className="card-body">
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-6">
-                    
-                    
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Date</span>
-                        </label>
-                        <textarea type="date" name="comment" placeholder="Comments here..." className="textarea textarea-bordered textarea-lg w-full" ></textarea>
-                        
+                    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-6">
+
+
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Date</span>
+                            </label>
+                            <textarea type="date" name="comment" placeholder="Comments here..." className="textarea textarea-bordered textarea-lg w-full" ></textarea>
+
+                        </div>
+
                     </div>
-                    
-                </div>
-                <div className="form-control mt-6">
-                    
-                    <input className="btn btn-primary btn-block" type="submit" value="Post" />
-                </div>
-            </form>
-                
+                    <div className="form-control mt-6">
+
+                        <input className="btn btn-primary btn-block" type="submit" value="Post" />
+                    </div>
+                </form>
+
             </div>
 
         </div>
