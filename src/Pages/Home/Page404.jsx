89f8034338870e8b0b1bcae2f38f9efdafd4119d
MohsenAlMahmud <mohsen.al.mahmud@gmail.com>
2023-11-08 04:57:31 +0600

    page 404 setup complete and i add a nice pic on this page

--- src/Pages/Home/Page404.jsx
+++ src/Pages/Home/Page404.jsx
@@ -3,10 +3,9 @@ import { Link } from "react-router-dom";
 
 const Page404 = () => {
     return (
-        <div className="flex justify-center text-center mt-64">
+        <div className="flex text-center mt-4">
             <div>
-                <h1 className="text-5xl font-bold p-4">Page Not Found</h1>
-                <h3 className="text-2xl font-semibold p-4">This URL is Not Correct</h3>
+                <img src="https://i.ibb.co/xKk65xr/004.jpg" alt="" />
                 <Link className="text-2xl font-semibold underline p-4">Go to Home</Link>
             </div>
         </div>
